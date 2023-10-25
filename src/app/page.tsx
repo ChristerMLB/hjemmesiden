e4479809c112c25bbea851d83b23387a5dee4c04
Christer M.L. Bendixen <gitstuff@fortelle.com>
2023-10-25 21:10:57 +0200

    added googly eyes, and scaled everything to focus on mobile screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,23 @@
 
 import { useEffect } from 'react';
 import Parallax from 'parallax-js';
-
 export default function Home() {
   useEffect(() => {
     var scene = document.getElementById('scene') as HTMLElement;
+    var leftEyeScene = document.getElementById('leftEyeScene') as HTMLElement;
+    var rightEyeScene = document.getElementById('rightEyeScene') as HTMLElement;
     var parallaxInstance = new Parallax(scene, {
       relativeInput: true,
+    });
+    var parallaxInstance2 = new Parallax(leftEyeScene, {
+      relativeInput: true,
+      limitX: 7,
+      limitY: 7,
+    });
+    var parallaxInstance3 = new Parallax(rightEyeScene, {
+      relativeInput: true,
+      limitX: 7,
+      limitY: 7,
     });
   }, []);
   return (
@@ -17,7 +28,14 @@ export default function Home() {
         data-depth='0.1'
       ></div>
       <div className='parallaxMeg overflow-hidden' data-depth='0.2'>
-        <div className='parallaxGooglyEyes'></div>
+        <div className='parallaxGooglyEyes'>
+          <div id='leftEyeScene' className='parallaxLeye parallaxEye'>
+            <div className='parallaxPupil' data-depth='-2'></div>
+          </div>
+          <div id='rightEyeScene' className='parallaxReye parallaxEye'>
+            <div className='parallaxPupil' data-depth='-2'></div>
+          </div>
+        </div>
       </div>
       <div className='parallaxForgrunn1 overflow-hidden' data-depth='0.4'></div>
       <div className='parallaxForgrunn2 overflow-hidden' data-depth='0.6'></div>
