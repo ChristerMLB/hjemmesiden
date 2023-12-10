d31954c9aa880f259c3d6be3a2c83713a9e8058b
Christer M.L.Bendixen <chribert@hotmail.com>
2023-12-10 19:34:53 +0100

    changed type for webarray as well

--- src/types/Web.ts
+++ src/types/Web.ts
@@ -1,2 +1,2 @@
-export type WebArray = [{index: number, navn:string, ingress: string, mertekst: string | null, teknologier: string, previewimg: string, previewimgalt: string, extraimg: string | null, extraimgalt: string|null, url: string | null}];
+export type WebArray = [{index: number, navn:string, ingress: string, mertekst: string | null, teknologier: string, previewimg: string, previewimgalt: string, extraimg: string | null, extraimgalt: string|null, url: string | null, gitHubUrl: string | null}];
 export type WebProject = {index: number, navn:string, ingress: string, mertekst: string | null, teknologier: string, previewimg: string, previewimgalt: string, extraimg: string | null, extraimgalt: string|null, url: string | null, gitHubUrl: string | null};
