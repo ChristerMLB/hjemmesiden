7640ac17f1c8d3cbe7e03b9e10b082b3d3c70db4
Christer M.L.Bendixen <chribert@hotmail.com>
2024-01-14 16:11:53 +0100

    styling av ressurskortene

--- src/app/bhg/BarnehageRessursKort.tsx
+++ src/app/bhg/BarnehageRessursKort.tsx
@@ -1,23 +1,28 @@
 import Image from "next/image";
 
 type RessursKortProps = {
-  tittel: string,
-  bilde: string,
-  altText: string,
-  oppsummering: string,
-}
+   tittel: string;
+   bilde: string;
+   altText: string;
+   oppsummering: string;
+};
 
-const BarnehageRessursKort = ({tittel, bilde, altText, oppsummering}:RessursKortProps) => {
-
-    return (
-        <div className="barnehageressurskort">
-          <div className="ressursheader">
-            <Image src={`/img/bhgressurser/${bilde}`} alt={altText} height={40} width={100} />
-            <h3>{tittel}</h3>
-          </div>
-          <p>{oppsummering}</p>
-        </div>
-    );
+const BarnehageRessursKort = ({ tittel, bilde, altText, oppsummering }: RessursKortProps) => {
+   return (
+      <div className="barnehageressurskort">
+         <Image
+            className="ressurshovedbilde"
+            src={`/img/bhgressurser/${bilde}`}
+            alt={altText}
+            height={40}
+            width={120}
+         />
+         <p>
+            <h3 className="ressurskorttittel">{tittel}</h3>
+            {oppsummering}
+         </p>
+      </div>
+   );
 };
 
 export default BarnehageRessursKort;
--- src/app/bhg/BhgRessurser.tsx
+++ src/app/bhg/BhgRessurser.tsx
@@ -3,12 +3,14 @@ import BarnehageRessursKort from "./BarnehageRessursKort";
 import { RessursKort } from "@/types/Ressurser";
 
 const BhgRessurser = () => {
-   const [ ressursListe, setRessursListe ] = useState<RessursKort[] | null>(null);
+   const [ressursListe, setRessursListe] = useState<RessursKort[] | null>(null);
 
    useEffect(() => {
       async function fetchRessurser() {
          try {
-            const fetchedRessurser = await fetch("api/ressursliste").then((response) => response.json());
+            const fetchedRessurser = await fetch("api/ressursliste").then((response) =>
+               response.json()
+            );
             setRessursListe(fetchedRessurser);
          } catch (e) {
             throw new Error(`Databasefeil: klarte ikke finne barnehageressursene: ${e}`);
@@ -17,15 +19,33 @@ const BhgRessurser = () => {
       fetchRessurser();
    }, []);
 
-   if(!ressursListe){ return <div className="barnehageressursliste">
-          <BarnehageRessursKort tittel='Laster ressurser...' bilde='' altText='' oppsummering='...' />
-   </div>; }
-
-   return (
-      <div className="barnehageressursliste">
-        {ressursListe?.map((ressurs) =>
-          <BarnehageRessursKort tittel={ressurs.tittel} bilde={ressurs.hovedbilde_url} altText={ressurs.hovedbilde_alttext} oppsummering={ressurs.oppsummering} key={ressurs.ressursid} />
-        )}
+   if (!ressursListe) {
+      return (
+         <div className="barnehageressursliste">
+            <BarnehageRessursKort
+               tittel="Laster ressurser..."
+               bilde=""
+               altText=""
+               oppsummering="..."
+               />
+         </div>
+      );
+    }
+    
+    return (
+      <div className="ressurslistewrapper">
+        <h2>Ressurser du kan laste ned</h2>
+        <div className="barnehageressursliste">
+           {ressursListe?.map((ressurs) => (
+              <BarnehageRessursKort
+                 tittel={ressurs.tittel}
+                 bilde={ressurs.hovedbilde_url}
+                 altText={ressurs.hovedbilde_alttext}
+                 oppsummering={ressurs.oppsummering}
+                 key={ressurs.ressursid}
+              />
+           ))}
+        </div>
       </div>
    );
 };
