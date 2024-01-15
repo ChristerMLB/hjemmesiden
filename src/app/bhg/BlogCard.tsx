070425ef79c6f224ff7561b7a1265bd0bc90a362
Christer M.L.Bendixen <chribert@hotmail.com>
2024-01-15 19:34:39 +0100

    more styling for the bhg-page. Pretty close to working on both big screens and small

--- src/app/bhg/BlogCard.tsx
+++ src/app/bhg/BlogCard.tsx
@@ -1,18 +1,22 @@
 import { BloggPost } from "@/types/Blogg";
 import parse from 'html-react-parser';
 import Image from "next/image";
+import { useEffect } from "react";
 
 type BlogCardProps = {
     post: BloggPost,
 }
 
 const BlogCard = ({post}:BlogCardProps) => {
+
+    const dato = new Date(post.dato);        
+
     return ( 
         <div className="blogcard">
             <div className="blogcardheader">
                 <Image src={`/img/blogg/${post.hovedbilde_url}`} alt={post.hovedbilde_alttext} width={1000} height={500} />
                 <h3>{post.tittel}</h3>
-                <aside>{post.dato.toLocaleString('nb-NO')}</aside>
+                <aside>{dato.toLocaleDateString('nb-NO')}</aside>
             </div>
             <div className="blogcardtext">
                 {parse(post.ingress)}
