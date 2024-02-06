a0c56c84c59b4eb15674239c4ad70c51f9232de3
Christer M.L.Bendixen <chribert@hotmail.com>
2024-02-06 21:16:16 +0100

    added security headers (fairly strict!)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self' '*.fortelle.no' 'https://fortelle.no'; font-src 'self' 'https://fonts.googleapis.com';"
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin'
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000'
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: "Camera=(); battery=(); geolocation=(); microphone=()"
+                    }
+                ]
+            
+            }
+        ]
+    },
+};
 
 module.exports = nextConfig;
