0c54380e2c90c8118565dbd0ad9a90c43b6c9303
Christer M.L.Bendixen <chribert@hotmail.com>
2024-02-20 20:47:48 +0100

    added some input validation with zod for the email address, would like to add the same for the message, and for it to do something more constructive than just have the form hang

--- pages/api/kontakt.ts
+++ pages/api/kontakt.ts
@@ -1,36 +1,43 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
+import { z } from "zod";
 
 const sendEpost = async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === 'POST') {
-        const {navn, epost, melding } = req.body;
+   if (req.method === "POST") {
+      try {
+         const { navn, epost, melding } = req.body;
 
-        const transporter = nodemailer.createTransport({
+         const emailSchema = z.string().email();
+         const parsedEmail = emailSchema.parse(epost);
+
+         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_SERVER,
             port: process.env.EMAIL_PORT,
             secure: true,
             auth: {
-                user: process.env.EMAIL_UNAME,
-                pass: process.env.EMAIL_SECRET_WORD,
-            }
-        });
+               user: process.env.EMAIL_UNAME,
+               pass: process.env.EMAIL_SECRET_WORD,
+            },
+         });
 
-        const info = await transporter.sendMail({
-            from: `"${navn}" <fraNettsiden@fortelle.no>`,
-            to: "fraNettsiden@fortelle.no",
-            subject: `Epost fra nettsiden fra ${navn} (${epost})`,
-            text: `${melding} \n\nSendt fra nettsiden, svar på ${epost}`,
-        })
+         const info = await transporter.sendMail({
+            from: `"${navn}" <franettsiden@fortelle.no>`,
+            to: "franettsiden@fortelle.no",
+            subject: `epost fra nettsiden fra ${navn} (${parsedEmail})`,
+            text: `${melding} \n\nsendt fra nettsiden, svar på ${parsedEmail}`,
+         });
 
-         //console.log('info:', info);
-         //console.log(req.body);
-         //console.log(`epostskjema mottatt, navn: ${navn}, epostadresse: ${epost}, melding: ${melding}`);
-
-        res.status(200).json({success: true});
-    }else{
-        res.status(405).json({ error: 'Forespørselen ble sendt med feil metode. Her er det noe galt på gang! Gi meg gjerne beskjed hvis du kan!'});
-    }
-}
+         res.status(200).json({ success: true });
+      } catch (e) {
+         res.status(400).json({
+            error: `det ser ut som det er noe feil med epostadressen: ${e}`,
+         });
+      }
+   } else {
+      res.status(405).json({
+         error: "forespørselen ble sendt med feil metode. her er det noe galt på gang! gi meg gjerne beskjed hvis du kan!",
+      });
+   }
+};
 
 export default sendEpost;
