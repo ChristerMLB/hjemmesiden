1ff5db3a5a213cf270b519d4cbc0eba2d96879cb
Christer M.L.Bendixen <chribert@hotmail.com>
2024-02-21 17:16:40 +0100

    escaping and parametrization for bloggposter.ts and added a NaN-check for both as well, since the parameter is supposed to be a number

--- pages/api/bloggposter.ts
+++ pages/api/bloggposter.ts
@@ -1,15 +1,20 @@
 import { pool } from "@/utilities/db";
 import { NextApiRequest, NextApiResponse } from "next";
+import { escape } from "validator";
 
 const bloggListe = async (req: NextApiRequest, res: NextApiResponse) => {
    let connection;
    try {
       const { forskyvning = 0 } = req.query;
+      const escapedForskyvning = parseInt(escape(forskyvning as string));
+      if (Number.isNaN(escapedForskyvning)) {
+         res.status(400).json({ error: '"forskyvning" kan bare være et heltall' });
+      }
       connection = await pool.promise().getConnection();
       const [bloggposter] = await connection.query(
-         `SELECT * FROM fortelle2.bloggposter ORDER BY dato LIMIT 10 OFFSET ${forskyvning}`
+         `SELECT * FROM fortelle2.bloggposter ORDER BY dato LIMIT 10 OFFSET ?`, [escapedForskyvning]
       );
-       console.log("bloggposter:", bloggposter);
+      console.log("bloggposter:", bloggposter);
       res.status(200).json(bloggposter);
    } catch (e) {
       res.status(500).json({ error: `En feil oppsto under oppslag i databasen: ${e}` });
--- pages/api/enkeltressurs.ts
+++ pages/api/enkeltressurs.ts
@@ -1,4 +1,3 @@
-
 import { pool } from "@/utilities/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { escape } from "validator";
@@ -7,9 +6,14 @@ const enkeltressurs = async (req: NextApiRequest, res: NextApiResponse) => {
    let connection;
    const { ressursId } = req.query;
    const escapedRessursId = parseInt(escape(ressursId as string));
+   if (Number.isNaN(escapedRessursId)) {
+      res.status(400).json({ error: "forskyvning kan bare være et heltall" });
+   }
    try {
       connection = await pool.promise().getConnection();
-      const [ressurs] = await connection.query(`SELECT * FROM fortelle2.ressurser WHERE id=?`, [escapedRessursId]);
+      const [ressurs] = await connection.query(`SELECT * FROM fortelle2.ressurser WHERE id=?`, [
+         escapedRessursId,
+      ]);
       res.status(200).json(ressurs);
    } catch (e) {
       res.status(500).json({ error: `En feil oppsto under oppslag i databasen: ${e}` });
