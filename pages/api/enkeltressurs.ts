83f3195da91ca15c71df58ed7ccf02bf937f8066
Christer M.L.Bendixen <chribert@hotmail.com>
2024-02-21 16:06:11 +0100

    escaping the query parameter in enkeltressurs to prevent xss

--- pages/api/enkeltressurs.ts
+++ pages/api/enkeltressurs.ts
@@ -1,14 +1,15 @@
 
 import { pool } from "@/utilities/db";
 import { NextApiRequest, NextApiResponse } from "next";
-import { useSearchParams } from "next/navigation";
+import { escape } from "validator";
 
 const enkeltressurs = async (req: NextApiRequest, res: NextApiResponse) => {
    let connection;
    const { ressursId } = req.query;
+   const escapedRessursId = parseInt(escape(ressursId as string));
    try {
       connection = await pool.promise().getConnection();
-      const [ressurs] = await connection.query(`SELECT * FROM fortelle2.ressurser WHERE id=${ressursId}`);
+      const [ressurs] = await connection.query(`SELECT * FROM fortelle2.ressurser WHERE id=${escapedRessursId}`);
       res.status(200).json(ressurs);
    } catch (e) {
       res.status(500).json({ error: `En feil oppsto under oppslag i databasen: ${e}` });
