218dc01b5a14e252608b1a49acdf66b466eb2f7e
Christer M.L.Bendixen <chribert@hotmail.com>
2024-03-02 20:43:27 +0100

    even more styling

--- src/app/bhg/BhgOmMeg.tsx
+++ src/app/bhg/BhgOmMeg.tsx
@@ -12,8 +12,7 @@ const BhgOmMeg = ({setKontaktModal, kontaktModal}:BhgOmMegTypes) => {
          <p>
             Hei, jeg er en barnehagelærer som også driver med webutvikling på si. På denne nettsiden
             har jeg samlet litt av det jeg har skrevet om utdanningspolitikk, noen av webprosjektene
-            mine, og noe av det jeg har laget av ressurser til barnehagebruk. Hvis det er noe du
-            lurer på, trykk på konvolutten for å ta kontakt:
+            mine, og noe av det jeg har laget av ressurser til barnehagebruk. Trykk gjerne på konvolutten for å ta kontakt:
             <KontaktKonvolutt setKontaktModal={setKontaktModal} kontaktModal={kontaktModal} />
          </p>
          <p>MVH • Christer M.L. Bendixen</p>
